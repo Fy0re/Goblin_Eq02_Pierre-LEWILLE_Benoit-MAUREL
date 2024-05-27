4c40cae2db07f933e4502b9c401cb52503a67b36
pierr <pierr@172.16.137.71>
2024-05-27 17:02:49 +0200

    Modif lecture_db

--- src/partie1_db/Lecture_db.java
+++ src/partie1_db/Lecture_db.java
@@ -7,28 +7,37 @@ import java.io.FileReader;
 import java.io.IOException;
 import com.opencsv.CSVReader;
 
+
+
 public class Lecture_db {
 
 	public static void main(String[] args) {
-		
-		List<Site> sites = new ArrayList<Site>();
-		
-		CSVReader reader = null;
-	    try {
-	    	//Créer fct pour selectionner fichier à lire
-	      reader = new CSVReader(new FileReader("Jeux_de_donnees"+File.separator+"petit"+File.separator+"init-sites-30-Carre.csv"));
-	      String[] nextLine;
-	      //read one line at a time  
-	      while ((nextLine = reader.readNext()) != null) {
-	        for (String token: nextLine) {
-	          System.out.println(token);
-	        }
-	        System.out.print("\n");
-	      }
-	    }
-	    catch(Exception e) {
-	      e.printStackTrace();
-	    }
-	}
 
+		List<Site> sites = new ArrayList<Site>();
+
+		CSVReader reader = null;
+		try {
+			//Créer fct pour selectionner fichier à lire
+			reader = new CSVReader(new FileReader("Jeux_de_donnees"+File.separator+"petit"+File.separator+"init-sites-30-Carre.csv"));
+			String[] nextLine;
+			int id, posx,posy;
+			// Ignorez la première ligne (les en-têtes)
+            reader.readNext();
+			//read one line at a time 
+			while ((nextLine = reader.readNext()) != null) { //readnext renvoie deja un tableau de valeur	
+				for (String token : nextLine) {
+					//sites.add(new sites(int idSite, int posX, int posY)); //Point pt1 = new Point(); et pt1 = new Point(1.0, 2.0);
+					id = Integer.parseInt(token.split(";")[0]);
+					posx = Integer.parseInt(token.split(";")[1]);
+					posy = Integer.parseInt(token.split(";")[2]);      
+					Site site = new Site(id, posx, posy);
+					sites.add(site);
+				}
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("sites : "+sites);
+	}
 }
